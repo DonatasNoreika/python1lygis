ddcf2d4c00547029a15120dfe0b7ac123c083f45
DonatasNoreika <donoras@gmail.com>
2020-09-11 21:35:18 +0300

    Update uzduotis_3-4.py

--- Programos/Programos - Masyvai, ciklai/uzduotis_3-4.py
+++ Programos/Programos - Masyvai, ciklai/uzduotis_3-4.py
@@ -1,17 +1,13 @@
-
-
 import random
 
 print("Bus sugeneruoti 3 skaičiai")
 print("Jei vienas iš jų – 5, tu pralaimėjai!")
 
-count = 0
-while count < 3:
+for x in range(3):
     num = random.randint(1, 6)
     print(num)
     if num == 5:
         print("Pralaimėjai...")
         break
-    count += 1
 else:
     print("Laimėjai!")
