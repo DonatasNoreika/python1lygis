b7b5015e8c09262de5dca6183c66091ab6033ec6
DonatasNoreika <donoras@gmail.com>
2020-11-24 17:31:18 +0200

    Update uzduotis_3-1.py

--- Programos/Programos - Masyvai, ciklai/uzduotis_3-1.py
+++ Programos/Programos - Masyvai, ciklai/uzduotis_3-1.py
@@ -1,8 +1,9 @@
-
-
 sarasas = [6, 98, 159, "zodziai", 5.55, True]
 
-print(sarasas.index(159))
+print(sarasas[2])
+
+sarasas.append(888)
+print(sarasas)
 
 sarasas.insert(2, "Naujas")
 print(sarasas)
@@ -12,3 +13,16 @@ print(sarasas)
 
 sarasas.clear()
 print(sarasas)
+
+amzius = {"Rokas": 20, "Andrius": 34, "Laura": 25}
+
+print(amzius["Laura"])
+
+amzius["Donatas"] = 100
+print(amzius)
+
+del amzius["Laura"]
+print(amzius)
+
+amzius["Andrius"] = 35
+print(amzius)
