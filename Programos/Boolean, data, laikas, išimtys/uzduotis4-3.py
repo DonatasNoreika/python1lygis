88ad6c8d374b5349a28aa8634dcc0883cfbc1953
DonatasNoreika <donoras@gmail.com>
2020-11-26 14:42:25 +0200

    Update uzduotis4-3.py

--- Programos/Boolean, data, laikas, išimtys/uzduotis4-3.py
+++ Programos/Boolean, data, laikas, išimtys/uzduotis4-3.py
@@ -7,9 +7,9 @@ now = datetime.datetime.now()
 skirtumas = now - ivesta_data
 
 print("Praėjo metų: ", skirtumas.days // 365)
-print("Praėjo mėnesių: ", skirtumas.days / 365 * 12)
+print("Praėjo mėnesių: ", round(skirtumas.days / 365 * 12))
 print("Praėjo savaičių: ", skirtumas.days // 7)
 print("Praėjo dienų: ", skirtumas.days)
-print("Praėjo valandų: ", skirtumas.total_seconds() / 3600)
-print("Praėjo minučių: ", skirtumas.total_seconds() / 60)
-print("Praėjo sekundžių: ", skirtumas.total_seconds())
+print("Praėjo valandų: ", round(skirtumas.total_seconds() / 3600))
+print("Praėjo minučių: ", round(skirtumas.total_seconds() / 60))
+print("Praėjo sekundžių: ", round(skirtumas.total_seconds()))
