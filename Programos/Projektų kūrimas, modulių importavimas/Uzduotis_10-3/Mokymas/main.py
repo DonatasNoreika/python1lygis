d7105a43496186b14ee82b2dc698f1d4958ebbd7
DonatasNoreika <donoras@gmail.com>
2020-12-02 09:58:32 +0200

    Update main.py

--- Programos/Projektų kūrimas, modulių importavimas/Uzduotis_10-3/Mokymas/main.py
+++ Programos/Projektų kūrimas, modulių importavimas/Uzduotis_10-3/Mokymas/main.py
@@ -1,6 +1,4 @@
-
-from modules.kursas import Kursas
-from modules.python_kursas import PythonKursas
+from modules.python_kursas import PythonKursas, Kursas
 
 kursas = Kursas("Fizika", "Domantas", 40)
 python_kursas = PythonKursas("Python", "Donatas", 80)
