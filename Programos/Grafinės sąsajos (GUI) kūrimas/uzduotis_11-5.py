557ecc3c38eadfb3d76c383a59cd969dae172119
DonatasNoreika <donoras@gmail.com>
2021-01-13 11:49:53 +0200

    Update uzduotis_11-5.py

--- Programos/Grafinės sąsajos (GUI) kūrimas/uzduotis_11-5.py
+++ Programos/Grafinės sąsajos (GUI) kūrimas/uzduotis_11-5.py
@@ -1,35 +1,31 @@
-
 # Perdaryti bet kurią ankstesnėse pamokose sukurtą programą, kurioje vartotojas turėjo įvesti duomenis, į programą su grafine sąsaja
 
 from tkinter import *
 import calendar
 
-pagrindinis_langas = Tk()
+langas = Tk()
 
-def tikrinti(event):
+def tikrinti():
     ivesta = int(ivedimas1.get())
-    print(type(ivesta))
     if calendar.isleap(ivesta):
         uzrasas2["text"] = (f"{ivesta} metai yra keliamieji!")
     else:
         uzrasas2["text"] = (f"{ivesta} metai yra nekeliamieji!")
 
-def uzdaryti(event):
-    pagrindinis_langas.withdraw()
-    sys.exit()
+def uzdaryti():
+    langas.destroy()
 
 # Laukų, mygtukų formavimas
-uzrasas1 = Label(pagrindinis_langas, text="Įveskite metus")
-ivedimas1 = Entry(pagrindinis_langas)
-mygtukas1 = Button(pagrindinis_langas, text="Patvirtinti")
-mygtukas1.bind("<Button-1>", tikrinti)
-ivedimas1.bind("<Return>", tikrinti)
-pagrindinis_langas.bind("<Escape>", uzdaryti)
-uzrasas2 = Label(pagrindinis_langas, text="")
+uzrasas1 = Label(langas, text="Įveskite metus")
+ivedimas1 = Entry(langas)
+mygtukas1 = Button(langas, text="Patvirtinti", command=tikrinti)
+ivedimas1.bind("<Return>", lambda event: tikrinti())
+langas.bind("<Escape>", lambda event: uzdaryti())
+uzrasas2 = Label(langas, text="")
 
 # Lango piešimas
 uzrasas1.grid(row=0, column=0)
 ivedimas1.grid(row=0, column=1)
 mygtukas1.grid(row=0, column=2)
 uzrasas2.grid(row=1, columnspan=3)
-pagrindinis_langas.mainloop()
+langas.mainloop()
