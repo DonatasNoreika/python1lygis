99edef222741c4df4e6481d72f173df21e113d7a
DonatasNoreika <donoras@gmail.com>
2021-01-13 11:48:54 +0200

    Update uzduotis_11-3.py

--- Programos/Grafinės sąsajos (GUI) kūrimas/uzduotis_11-3.py
+++ Programos/Grafinės sąsajos (GUI) kūrimas/uzduotis_11-3.py
@@ -1,4 +1,3 @@
-
 # Patobulinti 2 užduoties programą, kad ji:
 # Turėtų meniu, kuriame:
 # Būtų punktas "Išvalyti", kurį paspaudus išsitrintų tekstas eilutėje, kurioje spausdinamas pasisveikinimo tekstas
@@ -9,37 +8,28 @@
 
 from tkinter import *
 
-import sys
-
 pagrindinis_langas = Tk()
-paskutinis = "Nebuvo įvesta"
+paskutinis = StringVar()
 
-def pasisveikinti(event):
+def pasisveikinti():
     ivesta = ivedimas1.get()
     uzrasas2["text"] = (f"Labas, {ivesta}!")
-    global paskutinis
-    paskutinis = uzrasas2["text"]
+    paskutinis.set(uzrasas2["text"])
 
 def isvalyti():
     uzrasas2["text"] = ""
 
 def atkurti():
-    uzrasas2["text"] = paskutinis
+    uzrasas2["text"] = paskutinis.get()
 
-def uzdaryti(event):
-    pagrindinis_langas.withdraw()
-    sys.exit()
-
-def uzdaryti2():
-    pagrindinis_langas.withdraw()
-    sys.exit()
+def uzdaryti():
+    pagrindinis_langas.destroy()
 
 # Laukų, mygtukų formavimas
 uzrasas1 = Label(pagrindinis_langas, text="Įveskite vardą")
 ivedimas1 = Entry(pagrindinis_langas)
-mygtukas1 = Button(pagrindinis_langas, text="Patvirtinti")
-mygtukas1.bind("<Button-1>", pasisveikinti)
-ivedimas1.bind("<Return>", pasisveikinti)
+mygtukas1 = Button(pagrindinis_langas, text="Patvirtinti", command=pasisveikinti)
+ivedimas1.bind("<Return>", lambda event: pasisveikinti())
 uzrasas2 = Label(pagrindinis_langas, text="")
 
 # Meniu:
@@ -51,7 +41,7 @@ meniu.add_cascade(label="Meniu", menu=submeniu)
 submeniu.add_command(label="Išvalyti", command = isvalyti)
 submeniu.add_command(label="Atkurti paskutinį", command = atkurti)
 submeniu.add_separator()
-submeniu.add_command(label="Išeiti", command = uzdaryti2)
+submeniu.add_command(label="Išeiti", command = uzdaryti)
 
 # Lango piešimas
 uzrasas1.grid(row=0, column=0)
