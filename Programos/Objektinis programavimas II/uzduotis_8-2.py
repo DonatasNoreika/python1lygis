a8c280caff5f489c9c22261b701e49599bdc917d
DonatasNoreika <donoras@gmail.com>
2023-07-20 08:25:29 +0300

    Update uzduotis_8-2.py

--- Programos/Objektinis programavimas II/uzduotis_8-2.py
+++ Programos/Objektinis programavimas II/uzduotis_8-2.py
@@ -3,30 +3,25 @@ import datetime
 
 class Darbuotojas:
     def __init__(self, vardas, valandos_ikainis, dirba_nuo):
-        """
-        Darbuotojo objektas
-        :param vardas: darbuotojo vardas
-        :param valandos_ikainis: darbo valandos įkainis
-        :param dirba_nuo: datą įvesti formatu: YYYY-MM-DD
-        """
         self.vardas = vardas
         self.valandos_ikainis = valandos_ikainis
-        self.dirba_nuo = datetime.datetime.strptime(dirba_nuo, "%Y-%m-%d")
+        self.dirba_nuo = dirba_nuo
+        self.dirba_nuo_datetime = datetime.datetime.strptime(dirba_nuo, "%Y-%m-%d")
 
-    def _kiek_dienu(self):
-        skirtumas = datetime.datetime.today() - self.dirba_nuo
+    def _kiek_nudirbo_dienu(self):
+        skirtumas = datetime.datetime.today() - self.dirba_nuo_datetime
         return skirtumas.days
 
     def paskaiciuoti_atlyginima(self):
-        return self._kiek_dienu() * 8 * self.valandos_ikainis
+        return self._kiek_nudirbo_dienu() * 8 * self.valandos_ikainis
 
 
 class NormalusDarbuotojas(Darbuotojas):
-    def _kiek_dienu(self):
-        return super()._kiek_dienu() // 7 * 5
+    def _kiek_nudirbo_dienu(self):
+        return round(super()._kiek_nudirbo_dienu() / 7 * 5)
 
 
-darbuotojas1 = Darbuotojas("Jonas", 20, "2000-01-01")
-darbuotojas2 = NormalusDarbuotojas("Petras", 20, "2000-01-01")
+darbuotojas1 = Darbuotojas("Tomas", 20, "2000-12-12")
+darbuotojas2 = NormalusDarbuotojas("Domas", 20, "2000-12-12")
 print(darbuotojas1.paskaiciuoti_atlyginima())
 print(darbuotojas2.paskaiciuoti_atlyginima())
