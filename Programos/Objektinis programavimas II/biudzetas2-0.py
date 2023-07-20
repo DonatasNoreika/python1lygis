1009a42c2a9a5eb8e0d90b8a3ad07af9d96be991
DonatasNoreika <donoras@gmail.com>
2023-07-20 08:43:47 +0300

    Update biudzetas2-0.py

--- Programos/Objektinis programavimas II/biudzetas2-0.py
+++ Programos/Objektinis programavimas II/biudzetas2-0.py
@@ -2,73 +2,80 @@ class Irasas:
     def __init__(self, suma):
         self.suma = suma
 
+
 class PajamuIrasas(Irasas):
-    def __init__(self, suma, siuntejas, papildoma_info):
+    def __init__(self, suma, siuntejas, info):
         super().__init__(suma)
         self.siuntejas = siuntejas
-        self.papildoma_info = papildoma_info
+        self.info = info
+
+    def __str__(self):
+        return f"Pajamos: {self.suma} (siuntėjas - {self.siuntejas}, info - {self.info})"
+
 
 class IslaiduIrasas(Irasas):
-    def __init__(self, suma, atsiskaitymo_budas, isigyta_preke_paslauga):
+    def __init__(self, suma, atsiskaitymo_budas, isigyta, info):
         super().__init__(suma)
         self.atsiskaitymo_budas = atsiskaitymo_budas
-        self.isigyta_preke_paslauga = isigyta_preke_paslauga
+        self.isigyta = isigyta
+        self.info = info
 
-class Biudzetas():
+    def __str__(self):
+        return f"Išlaidos: {self.suma} (atsiskaitymo būdas - {self.atsiskaitymo_budas}, įsigyta - {self.isigyta}, info - {self.info})"
+
+
+class Biudzetas:
     def __init__(self):
         self.zurnalas = []
 
-    def ivesti_pajamas(self, suma, siuntejas, papildoma_informacija):
-        pajamu_irasas = PajamuIrasas(suma, siuntejas, papildoma_informacija)
-        self.zurnalas.append(pajamu_irasas)
+    def ivesti_pajamas(self):
+        suma = float(input("Suma: "))
+        siuntejas = input("Siuntėjas: ")
+        info = input("Papildoma informacija: ")
+        irasas = PajamuIrasas(suma, siuntejas, info)
+        self.zurnalas.append(irasas)
 
-    def ivesti_islaidas(self, suma, atsiskaitymo_budas, isigyta_preke_paslauga):
-        islaidu_irasas = IslaiduIrasas(suma, atsiskaitymo_budas, isigyta_preke_paslauga)
-        self.zurnalas.append(islaidu_irasas)
+    def ivesti_islaidas(self):
+        suma = float(input("Suma: "))
+        atsiskaitymo_budas = input("Atsiskaitymo būdas: ")
+        isigyta = input("Įsigyta prekė/paslauga: ")
+        info = input("Papildoma informacija: ")
+        irasas = IslaiduIrasas(suma, atsiskaitymo_budas, isigyta, info)
+        self.zurnalas.append(irasas)
 
-    def gauti_biudzeto_balansa(self):
-        balansas = 0
+    def ataskaita(self):
+        print("-----------------")
         for irasas in self.zurnalas:
-            if isinstance(irasas, PajamuIrasas):
-                balansas += irasas.suma
-            if isinstance(irasas, IslaiduIrasas):
-                balansas -= irasas.suma
-        return balansas
+            print(irasas)
 
-    def gauti_ataskaita(self):
+    def balansas(self):
+        suma = 0
         for irasas in self.zurnalas:
-            if isinstance(irasas, PajamuIrasas):
-                print(f"Pajamos: {irasas.suma} {irasas.siuntejas} {irasas.papildoma_info}")
-            if isinstance(irasas, IslaiduIrasas):
-                print(f"Išlaidos: {irasas.suma} {irasas.atsiskaitymo_budas} {irasas.isigyta_preke_paslauga}")
+            if type(irasas) is PajamuIrasas:
+                suma += irasas.suma
+            if type(irasas) is IslaiduIrasas:
+                suma -= irasas.suma
+        print("Balansas", suma)
 
-mano_biudzetas = Biudzetas()
+
+biudzetas = Biudzetas()
 
 while True:
-    print("Pasirinkite veiksmą: ")
-    print("1 - Įvesti pajamas")
-    print("2 - Įvesti išlaidas")
-    print("3 - Gauti balansą")
-    print("4 - Gauti ataskaitą")
-    print("0 - Išeiti iš programos")
-    pasirinkimas = input()
-    if pasirinkimas == "1":
-        print("Įveskite pajamas: ")
-        suma = int(input("Suma: "))
-        siuntejas = input("Siuntėjas: ")
-        papildoma_informacija = input("Papildoma informacija: ")
-        mano_biudzetas.ivesti_pajamas(suma, siuntejas, papildoma_informacija)
-        print("Pajamos įvestos sėkmingai!")
-    if pasirinkimas == "2":
-        print("Įveskite išlaidas: ")
-        suma = int(input("Suma: "))
-        atsiskaitymo_budas = input("Atsiskaitymo būdas: ")
-        isigyta_preke_paslauga = input("Įsigyta prekė/paslauga: ")
-        mano_biudzetas.ivesti_islaidas(suma, atsiskaitymo_budas, isigyta_preke_paslauga)
-        print("Išlaidos įvestos sėkmingai!")
-    if pasirinkimas == "3":
-        print(f"Sąskaitos balansas: {mano_biudzetas.gauti_biudzeto_balansa()}")
-    if pasirinkimas == "4":
-        mano_biudzetas.gauti_ataskaita()
-    if pasirinkimas == "0":
-        break
+    veiksmas = int(input("""
+    1 - įvesti pajamas
+    2 - įvesti išlaidas
+    3 - balansas
+    4 - ataskaita
+    5 - išeiti
+    """))
+    match veiksmas:
+        case 1:
+            biudzetas.ivesti_pajamas()
+        case 2:
+            biudzetas.ivesti_islaidas()
+        case 3:
+            biudzetas.balansas()
+        case 4:
+            biudzetas.ataskaita()
+        case 5:
+            break
