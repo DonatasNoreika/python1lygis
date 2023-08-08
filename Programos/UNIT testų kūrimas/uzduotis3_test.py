6aa2e462872a464ca05757f71fd16b0b783d68e4
DonatasNoreika <donoras@gmail.com>
2023-08-08 10:09:24 +0300

    Update uzduotis3_test.py

--- Programos/UNIT testų kūrimas/uzduotis3_test.py
+++ Programos/UNIT testų kūrimas/uzduotis3_test.py
@@ -1,10 +1,12 @@
 import unittest
-from uzduotis3 import kmi
+from kmi_skaiciavimas import kmi
+
+
 class TestSkaiciavimas(unittest.TestCase):
     def test_kmi(self):
-        self.assertEqual(kmi(78, 1.82), 23.54788069073783)
-        self.assertEqual(kmi(50, 1.56), 20.5456936226167)
-        self.assertEqual(kmi(100, 1.90), 27.70083102493075)
+        self.assertEqual(23.5, kmi(78, 1.82))
+        self.assertEqual(20.5, kmi(50, 1.56))
+        self.assertEqual(27.7, kmi(100, 1.90))
         with self.assertRaises(ValueError):
             kmi(20, 1.40)
         with self.assertRaises(ValueError):
