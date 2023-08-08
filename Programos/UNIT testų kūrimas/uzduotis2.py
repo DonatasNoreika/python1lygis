8a8e5102ccf6738aa847067b8ff331455c71d71b
DonatasNoreika <donoras@gmail.com>
2023-08-08 10:08:22 +0300

    Update uzduotis2.py

--- Programos/UNIT testų kūrimas/uzduotis2.py
+++ Programos/UNIT testų kūrimas/uzduotis2.py
@@ -1,43 +1,34 @@
-class StringTriukai():
-    def __init__(self, sakinys):
-        self.sakinys = sakinys
-
-    def pirmas_zodis(self):
-        zodziai = self.sakinys.split()
-        return zodziai[0]
-
-    def paskutinis_zodis(self):
-        zodziai = self.sakinys.split()
-        return zodziai[-1]
+class Sakinys:
+    def __init__(self, tekstas="Zen of Python"):
+        self.tekstas = tekstas
 
     def atbulai(self):
-        return self.sakinys[::-1]
-
-    def nuo_galo(self):
-        zodziai = self.sakinys.split()
-        atvirksciai = zodziai[::-1]
-        return " ".join(atvirksciai)
+        return self.tekstas[::-1]
 
     def didziosiomis(self):
-        return self.sakinys.upper()
+        return self.tekstas.upper()
 
     def mazosiomis(self):
-        return self.sakinys.lower()
+        return self.tekstas.lower()
+
+    def ieskoti(self, ieskomas):
+        return self.tekstas.count(ieskomas)
+
+    def pakeisti(self, senas, naujas):
+        return self.tekstas.replace(senas, naujas)
+
+    def atspausdinti_zodi(self, skaicius):
+        suskirstytas = self.tekstas.split()
+        return suskirstytas[skaicius]
 
     def info(self):
-        zodziai = len(self.sakinys.split())
-        skaiciai = sum(c.isdigit() for c in self.sakinys)
-        didziosios = sum(c.isupper() for c in self.sakinys)
-        mazosios = sum(c.islower() for c in self.sakinys)
-        return (f"Žodžių kiekis: {zodziai}, Skaičių kiekis: {skaiciai}, Didžiųjų raidžių: {didziosios}, Mažųjų raidžių: {mazosios}")
+        zodziu_kiekis = len(self.tekstas.split())
+        skaiciai = sum(i.isnumeric() for i in self.tekstas)
+        didziosios = sum(i.isupper() for i in self.tekstas)
+        mazosios = sum(i.islower() for i in self.tekstas)
+        print(
+            f"Žodžių kiekis: {zodziu_kiekis}, Skaičiai: {skaiciai}, Didžiosios: {didziosios}, Mažosios: {mazosios}")
+        return zodziu_kiekis, skaiciai, didziosios, mazosios
 
-
-sakinys = StringTriukai("Laba diena, Lietuva")
-
-print(sakinys.pirmas_zodis())
-print(sakinys.paskutinis_zodis())
-print(sakinys.atbulai())
-print(sakinys.nuo_galo())
-print(sakinys.didziosiomis())
-print(sakinys.mazosiomis())
-print(sakinys.info())
+    def __str__(self):
+        return ("Tekstas: " + self.tekstas)
