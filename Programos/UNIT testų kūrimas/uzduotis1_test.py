1f53fc05d2cf27a2d0b4f49fdd705e9df77be3ce
DonatasNoreika <donoras@gmail.com>
2023-08-08 09:02:38 +0300

    Update uzduotis1_test.py

--- Programos/UNIT testų kūrimas/uzduotis1_test.py
+++ Programos/UNIT testų kūrimas/uzduotis1_test.py
@@ -1,31 +1,61 @@
-
 import unittest
-from uzduotis1 import skaiciu_suma, didziausias_skaicius, sakinys_atvirksciai, ar_yra, patikrinti_sakini, lyginiai_skaiciai
+import datetime
+from funkcijos import (skaiciu_suma,
+                       saraso_suma,
+                       didziausias_skaicius,
+                       stringas_atbulai,
+                       info_apie_sakini,
+                       unikalus_sarasas,
+                       ar_pirminis,
+                       isrikiuoti_nuo_galo,
+                       ar_keliamieji,
+                       patikrinti_data)
 
-class TestUzduotis1(unittest.TestCase):
 
+class TestFunkcijos(unittest.TestCase):
     def test_skaiciu_suma(self):
-        self.assertEqual(skaiciu_suma(2, 6, 4, 78, 1, 454, 30, 68), 643)
-        self.assertEqual(skaiciu_suma(-2, 6, -4, -78, 1, 454, 0, 68), 445)
-        self.assertEqual(skaiciu_suma(-1000, 1000), 0)
+        self.assertEqual(45, skaiciu_suma(45, 5, -5))
+        self.assertEqual(62, skaiciu_suma(60, 2, 0))
+        self.assertEqual(50, skaiciu_suma(3, 3, 44))
 
+    def test_saraso_suma(self):
+        self.assertEqual(95, saraso_suma([4, 5, 78, 8]))
+        self.assertEqual(-68, saraso_suma([-20, -50, 2]))
+        self.assertEqual(202.70000000000005, saraso_suma([-1000, 2.5, 1200.200]))
 
     def test_didziausias_skaicius(self):
-        self.assertEqual(didziausias_skaicius(2, 5, 7, 9, 5, 12, 45, 900, 1000, 12), 1000)
+        self.assertEqual(78, didziausias_skaicius(5, 6, 78))
+        self.assertEqual(55, didziausias_skaicius(-300, 5, 55))
+        self.assertEqual(45.7, didziausias_skaicius(45.6, 45.7, 45.22))
 
-    def test_sakinys_atvirksciai(self):
-        self.assertEqual(sakinys_atvirksciai("Jau pavasaris atėjo, kas čia plytų tiek pridėjo?"), "pridėjo? tiek plytų čia kas atėjo, pavasaris Jau")
+    def test_stringas_atbulai(self):
+        self.assertEqual("satanoD", stringas_atbulai("Donatas"))
 
-    def test_ar_yra(self):
-        self.assertTrue(ar_yra(5, [5, 6, 7, 9, 10, 15, 45, 100]))
+    def test_info_apie_sakini(self):
+        self.assertEqual((5, 2, 28, 3), info_apie_sakini("Code Academy programavimo mokykla 101"))
 
-    def test_patikrinti_sakini(self):
-        lukestis = "Žodžių kiekis: 9, Skaičių kiekis: 2, Didžiųjų raidžių: 5, Mažųjų raidžių: 34"
-        self.assertEqual(patikrinti_sakini("12: Jau pavasaris atėjo, kas čia plytų TIEK pridėjo?"), lukestis)
+    def test_unikalus_sarasas(self):
+        self.assertCountEqual([4, 5, 'Labas', 6, True, 10], unikalus_sarasas([4, 5, "Labas", 6, "Labas", True, 5, True, 10]))
 
-    def test_lyginiai_skaiciai(self):
-        self.assertEqual(lyginiai_skaiciai(0, 51), [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50])
+    def test_ar_pirminis(self):
+        self.assertTrue(ar_pirminis(7))
+        self.assertFalse(ar_pirminis(8))
+        self.assertTrue(ar_pirminis(113))
 
+    def test_isrikiuoti_nuo_galo(self):
+        self.assertEqual("keturi trys du Vienas", isrikiuoti_nuo_galo("Vienas du trys keturi"))
 
+    def test_ar_keliamieji(self):
+        self.assertTrue(ar_keliamieji(2000))
+        self.assertTrue(ar_keliamieji(1996))
+        self.assertFalse(ar_keliamieji(1994))
+        self.assertFalse(ar_keliamieji(2100))
 
+    def test_patikrinti_data(self):
+        lukestis = (23, 283, 1231, 8619, 206877, 12412608, 744756468)
+        rezultatas = patikrinti_data(sukaktis="2000-01-01 12:12:12", dabar=datetime.datetime(2023, 8, 8, 9, 0, 0))
+        self.assertEqual(lukestis, rezultatas)
 
+        lukestis2 = (32, 389, 1691, 11837, 284109, 17046528, 1022791668)
+        rezultatas2 = patikrinti_data(sukaktis="1991-03-11 12:12:12", dabar=datetime.datetime(2023, 8, 8, 9, 0, 0))
+        self.assertEqual(lukestis2, rezultatas2)
