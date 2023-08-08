260e750d0d8a8c33d0f29fc304b569e8978fcf63
DonatasNoreika <donoras@gmail.com>
2023-08-08 10:08:48 +0300

    Update uzduotis2_test.py

--- Programos/UNIT testų kūrimas/uzduotis2_test.py
+++ Programos/UNIT testų kūrimas/uzduotis2_test.py
@@ -1,33 +1,31 @@
-
 import unittest
-from uzduotis2 import StringTriukai
+from sakinys import Sakinys
 
-class TestUzduotis14_2(unittest.TestCase):
 
+class TestSakinys(unittest.TestCase):
     def setUp(self):
-        self.objektas = StringTriukai("Laba diena, Lietuva")
-
-    def test_pirmas_zodis(self):
-        self.assertEqual(self.objektas.pirmas_zodis(), "Laba")
-
-    def test_paskutinis_zodis(self):
-        self.assertEqual(self.objektas.paskutinis_zodis(), "Lietuva")
+        self.objektas1 = Sakinys()
+        self.objektas2 = Sakinys("Code Academy programavimo mokykla 101")
 
     def test_atbulai(self):
-        self.assertEqual(self.objektas.atbulai(), "avuteiL ,aneid abaL")
-
-    def test_nuo_galo(self):
-        self.assertEqual(self.objektas.nuo_galo(), "Lietuva diena, Laba")
+        self.assertEqual("101 alkykom omivamargorp ymedacA edoC", self.objektas2.atbulai())
+        self.assertEqual("nohtyP fo neZ", self.objektas1.atbulai())
 
     def test_didziosiomis(self):
-        self.assertEqual(self.objektas.didziosiomis(), "LABA DIENA, LIETUVA")
+        self.assertEqual("CODE ACADEMY PROGRAMAVIMO MOKYKLA 101", self.objektas2.didziosiomis())
 
     def test_mazosiomis(self):
-        self.assertEqual(self.objektas.mazosiomis(), "laba diena, lietuva")
+        self.assertEqual("code academy programavimo mokykla 101", self.objektas2.mazosiomis())
+
+    def test_ieskoti(self):
+        self.assertEqual(4, self.objektas2.ieskoti("a"))
+
+    def test_pakeisti(self):
+        self.assertEqual("Code Academy muzikos mokykla 101", self.objektas2.pakeisti("programavimo", "muzikos"))
+        self.assertEqual("Code Academy programavimo universitetas 101", self.objektas2.pakeisti("mokykla", "universitetas"))
+
+    def test_atspausdinti_zodi(self):
+        self.assertEqual("programavimo", self.objektas2.atspausdinti_zodi(2))
 
     def test_info(self):
-        lukestis = "Žodžių kiekis: 3, Skaičių kiekis: 0, Didžiųjų raidžių: 2, Mažųjų raidžių: 14"
-        self.assertEqual(self.objektas.info(), lukestis)
-
-
-
+        self.assertEqual((5, 3, 2, 28), self.objektas2.info())
