60340dc2895fbaedc55fc94ed6bed7276e549aa0
DonatasNoreika <donoras@gmail.com>
2023-08-08 10:09:46 +0300

    Update uzduotis3.py

--- Programos/UNIT testų kūrimas/uzduotis3.py
+++ Programos/UNIT testų kūrimas/uzduotis3.py
@@ -1,10 +1,6 @@
 def kmi(svoris, ugis):
-    if svoris < 30:
-        raise ValueError("Svoris per mažas")
-    if svoris > 200:
-        raise ValueError("Svoris per didelis")
-    if ugis < 1.0:
-        raise ValueError("Ūgis per mažas")
-    if ugis > 3.0:
-        raise ValueError("Ūgis per didelis")
-    return svoris / (ugis**2)
+    if svoris <= 20 or svoris >= 240:
+        raise ValueError
+    if ugis <= 0.40 or ugis >= 3.40:
+        raise ValueError
+    return round(svoris / ugis ** 2, 1)
