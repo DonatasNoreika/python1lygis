45164382ba305a4ec77563c7257ba8ab25ebc8fa
DonatasNoreika <donoras@gmail.com>
2025-01-17 08:36:49 +0200

    Update asmens_kodas.py

--- Programos/Funkcijos/asmens_kodas.py
+++ Programos/Funkcijos/asmens_kodas.py
@@ -1,33 +1,23 @@
-def gauti_ak_kontrolini(kodas):
-    kodas_str = str(kodas)
-    A = int(kodas_str[0])
-    B = int(kodas_str[1])
-    C = int(kodas_str[2])
-    D = int(kodas_str[3])
-    E = int(kodas_str[4])
-    F = int(kodas_str[5])
-    G = int(kodas_str[6])
-    H = int(kodas_str[7])
-    I = int(kodas_str[8])
-    J = int(kodas_str[9])
-    S = A * 1 + B * 2 + C * 3 + D * 4 + E * 5 + F * 6 + G * 7 + H * 8 + I * 9 + J * 1
+def gauti_ak_kontrolini(asmens_kodas: str | int) -> int:
+    a, b, c, d, e, f, g, h, i, j = map(int, list(str(asmens_kodas)[:-1]))
+    S = a * 1 + b * 2 + c * 3 + d * 4 + e * 5 + f * 6 + g * 7 + h * 8 + i * 9 + j * 1
     if S % 11 != 10:
-        kontrolinis = S % 11
+        return S % 11
     else:
-        S = A * 3 + B * 4 + C * 5 + D * 6 + E * 7 + F * 8 + G * 9 + H * 1 + I * 2 + J * 3
+        S = a * 3 + b * 4 + c * 5 + d * 6 + e * 7 + f * 8 + g * 9 + h * 1 + i * 2 + j * 3
         if S % 11 != 10:
-            kontrolinis = S % 11
+            return S % 11
         else:
-            kontrolinis = 0
-    return kontrolinis
-
-def ak_validavimas(kodas):
-    K = int(str(kodas)[-1])
-    return gauti_ak_kontrolini(kodas) == K
+            return 0
 
 
-# print(ak_validavimas(40204219251))
-# print(ak_validavimas(49508079556))
+def ar_ak_validus(asmens_kodas: str | int) -> bool:
+    return int(str(asmens_kodas)[-1]) == gauti_ak_kontrolini(asmens_kodas)
+
+
+print(ar_ak_validus(45212265291))
+print(ar_ak_validus(43311245363))
+
 
 def ak_generavimas(lytis, data, eilesnr):
     data_split = data.split("-")
